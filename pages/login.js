6d57cc4043caf965f19a7f02b44e276fc985cc41
refrain62 <nao_h13@hotmail.com>
2023-12-26 19:33:58 +0900

    oauth login

---
+++ pages/login.js
@@ -0,0 +1,50 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from 'next/router';
+
+
+export default function Login() {
+  const { data: session } = useSession()
+  const router = useRouter();
+
+  if (session) {
+    return (
+      <div>
+        <h1 className="title">Create Next App</h1>
+        <div>
+            <h2>
+                Signed in as {session.user.email} <br />
+            </h2> 
+            <div>
+            <button
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => router.push('/Profile')}
+                >
+                 User Profile
+            </button>
+            <button
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => { signOut() }}
+                >
+                 Sign out
+             </button>
+              </div>
+        </div>
+      </div>
+    )
+  }
+  
+  return (
+    <div>
+        <h1 className="title">Create Next App</h1>
+        <div>
+            <h2> You are not signed in!!</h2>
+            <button
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => signIn()}
+                >
+                    Sign in
+                </button>
+        </div>
+    </div>
+  )
+}
